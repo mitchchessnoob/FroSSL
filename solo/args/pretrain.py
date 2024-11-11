833d9e834cc693124b246c9cf0e3920798529f89
mitchchessnoob <michele.narese@gmail.com>
2024-11-11 19:02:27 +0100

    Update pretrain.py

--- solo/args/pretrain.py
+++ solo/args/pretrain.py
@@ -27,6 +27,7 @@ _N_CLASSES_PER_DATASET = {
     "imagenet": 1000,
     "imagenet100": 100,
     "tiny-imagenet": 200,
+    "EuroSAT": 10
 }
 
 _SUPPORTED_DATASETS = [
@@ -37,6 +38,7 @@ _SUPPORTED_DATASETS = [
     "imagenet100",
     "tiny-imagenet",
     "custom",
+    "EuroSAT"
 ]
 
 
