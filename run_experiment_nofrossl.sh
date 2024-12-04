78d4177682481a0c695f16517a2b0022cd910f23
hannscook <hanns.hahn@hotmail.de>
2024-12-04 13:58:28 +0100

    ivne exp

--- run_experiment_nofrossl.sh
+++ run_experiment_nofrossl.sh
@@ -1,4 +1,4 @@
-echo "run experiments."
+echo "run mmcr."
 
 EXPERIMENT_NAME="mmcr_eurosat"
 DATASET="eurosat_msi"
@@ -13,7 +13,7 @@ python3 -u main_pretrain.py \
     ++backbone.name="$BACKBONE"  \
     ++data.dataset="$DATASET" 
 
-get pretrained path from last_ckpt.txt file
+# get pretrained path from last_ckpt.txt file
 TRAINED_CHECKPOINT_PATH=$(cat last_ckpt.txt)
 TRAINED_CHECKPOINT_WANDB_ID=$(echo $TRAINED_CHECKPOINT_PATH | awk -F '/' '{print $3}')
 echo "$TRAINED_CHECKPOINT_WANDB_ID $TRAINED_CHECKPOINT_PATH"
@@ -24,5 +24,35 @@ python3 -u main_linear.py \
     --config-name "$CONFIG_NAME" \
     ++data.dataset="$DATASET" \
     ++name="$EXPERIMENT_NAME-linear-$TRAINED_CHECKPOINT_WANDB_ID" \
+
+
+echo "run ivne."
+
+EXPERIMENT_NAME="ivne_eurosat"
+DATASET="eurosat_msi"
+BACKBONE="resnet50" 
+CONFIG_NAME="ivne"
+
+
+python3 -u main_pretrain.py \
+    --config-path "scripts/pretrain/eurosat_msi" \
+    --config-name "$CONFIG_NAME" \
+    ++name="$EXPERIMENT_NAME-pretrain" \
+    ++backbone.name="$BACKBONE"  \
+    ++data.dataset="$DATASET" 
+
+# get pretrained path from last_ckpt.txt file
+TRAINED_CHECKPOINT_PATH=$(cat last_ckpt.txt)
+TRAINED_CHECKPOINT_WANDB_ID=$(echo $TRAINED_CHECKPOINT_PATH | awk -F '/' '{print $3}')
+echo "$TRAINED_CHECKPOINT_WANDB_ID $TRAINED_CHECKPOINT_PATH"
+
+
+python3 -u main_linear.py \
+    --config-path "scripts/linear/$DATASET"  \
+    --config-name "$CONFIG_NAME" \
+    ++data.dataset="$DATASET" \
+    ++name="$EXPERIMENT_NAME-linear-$TRAINED_CHECKPOINT_WANDB_ID" \
+
+
 
 echo "done"
