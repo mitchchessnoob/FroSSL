40f92dbb5b30124c91929ff959fef3f5d6cd8936
mitchchessnoob <michele.narese@gmail.com>
2024-12-07 09:12:54 +0100

    Update main_semiSL.py

--- main_semiSL.py
+++ main_semiSL.py
@@ -41,7 +41,7 @@ def main(configs_path, augments_path, key):
         # Create CfgNode
         augments = OmegaConf.create(yaml_data)
         # Initialize wandb
-        wandb.init(project=configs.name, config=configs, name = f"semiSL with weight {configs.ssl_weight}")
+        wandb.init(project=configs.name, config=configs)
 
         # Set device
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
