d3cf8fd2756c2e8bd673f37bf0b82371b530cab5
hannscook <hanns.hahn@hotmail.de>
2024-12-07 18:21:04 +0100

    linux fix

--- solo/data/download_mit.py
+++ solo/data/download_mit.py
@@ -9,18 +9,18 @@ def download_mit_dataset():
 
     path = kagglehub.dataset_download("itsahmad/indoor-scenes-cvpr-2019")
     current_dir = os.getcwd()
-    shutil.move(path, current_dir+"\\datasets\\mit67")
+    shutil.move(path, current_dir+"/datasets/mit67")
 
-    with open(current_dir+"\\datasets\\mit67\\TrainImages.txt", 'r') as f:
+    with open(current_dir+"/datasets/mit67/TrainImages.txt", 'r') as f:
         train_files = f.readlines()
     train_files = [fname[:-1].split("/") for fname in train_files[:-1]] +[train_files[-1].split("/")]
 
-    with open(current_dir+"\\datasets\\mit67\\TestImages.txt", 'r') as f:
+    with open(current_dir+"/datasets/mit67/TestImages.txt", 'r') as f:
         test_files = f.readlines()
     test_files = [fname[:-1].split("/") for fname in test_files[:-1]] +[test_files[-1].split("/")]
 
-    move_images(current_dir+"\\datasets\\mit67\\indoorCVPR_09\\Images", train_files, current_dir+"\\datasets\\mit67\\train_images")
-    move_images(current_dir+"\\datasets\\mit67\\indoorCVPR_09\\Images", test_files, current_dir+"\\datasets\\mit67\\test_images")
+    move_images(current_dir+"/datasets/mit67/indoorCVPR_09/Images", train_files, current_dir+"/datasets/mit67/train_images")
+    move_images(current_dir+"/datasets/mit67/indoorCVPR_09/Images", test_files, current_dir+"/datasets/mit67/test_images")
 
 
 def move_images(images_dir, file_list, target_dir):
