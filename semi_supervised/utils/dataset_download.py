02b5a2956f99979dd4a0fbd2357bdddfee1759f3
mitchchessnoob <michele.narese@gmail.com>
2024-12-07 08:41:52 +0100

    Create dataset_download.py

---
+++ semi_supervised/utils/dataset_download.py
@@ -0,0 +1,11 @@
+import kagglehub
+import shutil
+def dataset()
+  # Download latest version
+  path = kagglehub.dataset_download("mei1963/office31")
+
+
+  destination_path = '.'
+  
+  # Move the folder
+  shutil.move(path, destination_path)
