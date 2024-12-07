e43472d4e5e358b71922ff0bfff74396ee9d56db
mitchchessnoob <michele.narese@gmail.com>
2024-12-07 09:06:51 +0100

    Update main_SL.sh

--- main_SL.sh
+++ main_SL.sh
@@ -1,17 +1,15 @@
 echo "run frossl SemiSL experiment."
 
-KAGGLE_USERNAME="michelenarese"
-KAGGLE_KEY="0a593cdaf1707de5e0924de7be17019a"
+export KAGGLE_USERNAME="michelenarese"
+export KAGGLE_KEY="0a593cdaf1707de5e0924de7be17019a"
 
 CONFIGS_PATH=/content/FroSSL/semi_supervised/configs.yaml
 AUGMENTS_PATH=/content/FroSSL/scripts/pretrain/office31/augmentations/asymmetric.yaml
-KEY="57e49312fc462a736d24abd32cc7891d91258b76" 
-
-
+KEY="57e49312fc462a736d24abd32cc7891d91258b76"
 
 python3 -u main_semiSL.py \
-    -config_path $CONFIGS_PATH \
+    --config_path $CONFIGS_PATH \
     --augments_path $AUGMENTS_PATH \
-    --key=$KEY \
+    --key=$KEY
 
 echo "done"
