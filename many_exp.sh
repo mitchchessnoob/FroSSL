e4d3e23e5d79fec5f2254ad9774db7f770d2a20b
hannscook <hanns.hahn@hotmail.de>
2024-12-07 19:31:34 +0100

    many exp

--- many_exp.sh
+++ many_exp.sh
@@ -1,13 +1,13 @@
-
-EXPERIMENT_NAME="eurosat_final_model"
-DATASET="eurosat_msi"
+EXPERIMENT_NAME="mit67_sym"
+DATASET="mit67"
 BACKBONE="resnet50" 
-CONFIG_NAME="frossl"
+CONFIG_NAME="frossl_sym"
 
-echo "run $CONFIG_NAME."
+echo "run $CONFIG_NAME experiment with $DATASET."
+
 
 python3 -u main_pretrain.py \
-    --config-path "scripts/pretrain/eurosat_msi" \
+    --config-path "scripts/pretrain/mit67" \
     --config-name "$CONFIG_NAME" \
     ++name="$EXPERIMENT_NAME-pretrain" \
     ++backbone.name="$BACKBONE"  \
@@ -25,23 +25,25 @@ python3 -u main_linear.py \
     ++data.dataset="$DATASET" \
     ++name="$EXPERIMENT_NAME-linear-$TRAINED_CHECKPOINT_WANDB_ID" \
 
-echo compute test accuracies
-
 python3 -c "from main_test import main; main('scripts/linear/$DATASET/$CONFIG_NAME.yaml')"
 
 echo "done"
 
-########################
 
-EXPERIMENT_NAME="eurosat_a15"
-DATASET="eurosat_msi"
+
+##############
+
+
+EXPERIMENT_NAME="mit67_wsym"
+DATASET="mit67"
 BACKBONE="resnet50" 
-CONFIG_NAME="frossl_a15"
+CONFIG_NAME="frossl_wsym"
 
-echo "run $CONFIG_NAME."
+echo "run $CONFIG_NAME experiment with $DATASET."
+
 
 python3 -u main_pretrain.py \
-    --config-path "scripts/pretrain/eurosat_msi" \
+    --config-path "scripts/pretrain/mit67" \
     --config-name "$CONFIG_NAME" \
     ++name="$EXPERIMENT_NAME-pretrain" \
     ++backbone.name="$BACKBONE"  \
@@ -59,200 +61,66 @@ python3 -u main_linear.py \
     ++data.dataset="$DATASET" \
     ++name="$EXPERIMENT_NAME-linear-$TRAINED_CHECKPOINT_WANDB_ID" \
 
-echo compute test accuracies
+python3 -c "from main_test import main; main('scripts/linear/$DATASET/$CONFIG_NAME.yaml')"
+
+echo "done"
+
+
+
+############
+
+
+EXPERIMENT_NAME="mit67_asym"
+DATASET="mit67"
+BACKBONE="resnet50" 
+CONFIG_NAME="frossl_asym"
+
+echo "run $CONFIG_NAME experiment with $DATASET."
+
+
+python3 -u main_pretrain.py \
+    --config-path "scripts/pretrain/mit67" \
+    --config-name "$CONFIG_NAME" \
+    ++name="$EXPERIMENT_NAME-pretrain" \
+    ++backbone.name="$BACKBONE"  \
+    ++data.dataset="$DATASET" 
+
+get pretrained path from last_ckpt.txt file
+TRAINED_CHECKPOINT_PATH=$(cat last_ckpt.txt)
+TRAINED_CHECKPOINT_WANDB_ID=$(echo $TRAINED_CHECKPOINT_PATH | awk -F '/' '{print $3}')
+echo "$TRAINED_CHECKPOINT_WANDB_ID $TRAINED_CHECKPOINT_PATH"
+
+
+python3 -u main_linear.py \
+    --config-path "scripts/linear/$DATASET"  \
+    --config-name "$CONFIG_NAME" \
+    ++data.dataset="$DATASET" \
+    ++name="$EXPERIMENT_NAME-linear-$TRAINED_CHECKPOINT_WANDB_ID" \
 
 python3 -c "from main_test import main; main('scripts/linear/$DATASET/$CONFIG_NAME.yaml')"
 
 echo "done"
 
 
-########################
+############
 
-EXPERIMENT_NAME="eurosat_mc"
-DATASET="eurosat_msi"
+
+EXPERIMENT_NAME="mit67_mc"
+DATASET="mit67"
 BACKBONE="resnet50" 
 CONFIG_NAME="frossl_mc"
 
-echo "run $CONFIG_NAME."
+echo "run $CONFIG_NAME experiment with $DATASET."
+
 
 python3 -u main_pretrain.py \
-    --config-path "scripts/pretrain/eurosat_msi" \
+    --config-path "scripts/pretrain/mit67" \
     --config-name "$CONFIG_NAME" \
     ++name="$EXPERIMENT_NAME-pretrain" \
     ++backbone.name="$BACKBONE"  \
     ++data.dataset="$DATASET" 
 
 get pretrained path from last_ckpt.txt file
-TRAINED_CHECKPOINT_PATH=$(cat last_ckpt.txt)
-TRAINED_CHECKPOINT_WANDB_ID=$(echo $TRAINED_CHECKPOINT_PATH | awk -F '/' '{print $3}')
-echo "$TRAINED_CHECKPOINT_WANDB_ID $TRAINED_CHECKPOINT_PATH"
-
-
-python3 -u main_linear.py \
-    --config-path "scripts/linear/$DATASET"  \
-    --config-name "$CONFIG_NAME" \
-    ++data.dataset="$DATASET" \
-    ++name="$EXPERIMENT_NAME-linear-$TRAINED_CHECKPOINT_WANDB_ID" \
-
-echo compute test accuracies
-
-python3 -c "from main_test import main; main('scripts/linear/$DATASET/$CONFIG_NAME.yaml')"
-
-echo "done"
-
-
-########################
-
-EXPERIMENT_NAME="eurosat_mv4"
-DATASET="eurosat_msi"
-BACKBONE="resnet50" 
-CONFIG_NAME="frossl_mv4"
-
-echo "run $CONFIG_NAME."
-
-python3 -u main_pretrain.py \
-    --config-path "scripts/pretrain/eurosat_msi" \
-    --config-name "$CONFIG_NAME" \
-    ++name="$EXPERIMENT_NAME-pretrain" \
-    ++backbone.name="$BACKBONE"  \
-    ++data.dataset="$DATASET" 
-
-get pretrained path from last_ckpt.txt file
-TRAINED_CHECKPOINT_PATH=$(cat last_ckpt.txt)
-TRAINED_CHECKPOINT_WANDB_ID=$(echo $TRAINED_CHECKPOINT_PATH | awk -F '/' '{print $3}')
-echo "$TRAINED_CHECKPOINT_WANDB_ID $TRAINED_CHECKPOINT_PATH"
-
-
-python3 -u main_linear.py \
-    --config-path "scripts/linear/$DATASET"  \
-    --config-name "$CONFIG_NAME" \
-    ++data.dataset="$DATASET" \
-    ++name="$EXPERIMENT_NAME-linear-$TRAINED_CHECKPOINT_WANDB_ID" \
-
-echo compute test accuracies
-
-python3 -c "from main_test import main; main('scripts/linear/$DATASET/$CONFIG_NAME.yaml')"
-
-echo "done"
-
-
-########################
-
-EXPERIMENT_NAME="eurosat_rt45"
-DATASET="eurosat_msi"
-BACKBONE="resnet50" 
-CONFIG_NAME="frossl_rt45"
-
-echo "run $CONFIG_NAME."
-
-python3 -u main_pretrain.py \
-    --config-path "scripts/pretrain/eurosat_msi" \
-    --config-name "$CONFIG_NAME" \
-    ++name="$EXPERIMENT_NAME-pretrain" \
-    ++backbone.name="$BACKBONE"  \
-    ++data.dataset="$DATASET" 
-
-get pretrained path from last_ckpt.txt file
-TRAINED_CHECKPOINT_PATH=$(cat last_ckpt.txt)
-TRAINED_CHECKPOINT_WANDB_ID=$(echo $TRAINED_CHECKPOINT_PATH | awk -F '/' '{print $3}')
-echo "$TRAINED_CHECKPOINT_WANDB_ID $TRAINED_CHECKPOINT_PATH"
-
-
-python3 -u main_linear.py \
-    --config-path "scripts/linear/$DATASET"  \
-    --config-name "$CONFIG_NAME" \
-    ++data.dataset="$DATASET" \
-    ++name="$EXPERIMENT_NAME-linear-$TRAINED_CHECKPOINT_WANDB_ID" \
-
-echo compute test accuracies
-
-python3 -c "from main_test import main; main('scripts/linear/$DATASET/$CONFIG_NAME.yaml')"
-
-echo "done"
-
-
-
-########   LONGRUNS #########
-
-EXPERIMENT_NAME="eurosat_ivne"
-DATASET="eurosat_msi"
-BACKBONE="resnet50" 
-CONFIG_NAME="ivne"
-
-echo "run $CONFIG_NAME."
-
-python3 -u main_pretrain.py \
-    --config-path "scripts/pretrain/eurosat_msi" \
-    --config-name "$CONFIG_NAME" \
-    ++name="$EXPERIMENT_NAME-pretrain" \
-    ++backbone.name="$BACKBONE"  \
-    ++data.dataset="$DATASET" 
-
-# get pretrained path from last_ckpt.txt file
-TRAINED_CHECKPOINT_PATH=$(cat last_ckpt.txt)
-TRAINED_CHECKPOINT_WANDB_ID=$(echo $TRAINED_CHECKPOINT_PATH | awk -F '/' '{print $3}')
-echo "$TRAINED_CHECKPOINT_WANDB_ID $TRAINED_CHECKPOINT_PATH"
-
-
-python3 -u main_linear.py \
-    --config-path "scripts/linear/$DATASET"  \
-    --config-name "$CONFIG_NAME" \
-    ++data.dataset="$DATASET" \
-    ++name="$EXPERIMENT_NAME-linear-$TRAINED_CHECKPOINT_WANDB_ID" \
-
-python3 -c "from main_test import main; main('scripts/linear/$DATASET/$CONFIG_NAME.yaml')"
-
-echo "done"
-
-
-
-EXPERIMENT_NAME="eurosat_mmcr"
-DATASET="eurosat_msi"
-BACKBONE="resnet50" 
-CONFIG_NAME="mmcr"
-
-echo "run $CONFIG_NAME."
-
-python3 -u main_pretrain.py \
-    --config-path "scripts/pretrain/eurosat_msi" \
-    --config-name "$CONFIG_NAME" \
-    ++name="$EXPERIMENT_NAME-pretrain" \
-    ++backbone.name="$BACKBONE"  \
-    ++data.dataset="$DATASET" 
-
-# get pretrained path from last_ckpt.txt file
-TRAINED_CHECKPOINT_PATH=$(cat last_ckpt.txt)
-TRAINED_CHECKPOINT_WANDB_ID=$(echo $TRAINED_CHECKPOINT_PATH | awk -F '/' '{print $3}')
-echo "$TRAINED_CHECKPOINT_WANDB_ID $TRAINED_CHECKPOINT_PATH"
-
-
-python3 -u main_linear.py \
-    --config-path "scripts/linear/$DATASET"  \
-    --config-name "$CONFIG_NAME" \
-    ++data.dataset="$DATASET" \
-    ++name="$EXPERIMENT_NAME-linear-$TRAINED_CHECKPOINT_WANDB_ID" \
-
-python3 -c "from main_test import main; main('scripts/linear/$DATASET/$CONFIG_NAME.yaml')"
-
-echo "done"
-
-
-
-EXPERIMENT_NAME="eurosat_simclr"
-DATASET="eurosat_msi"
-BACKBONE="resnet50" 
-CONFIG_NAME="simclr"
-
-echo "run $CONFIG_NAME."
-
-python3 -u main_pretrain.py \
-    --config-path "scripts/pretrain/eurosat_msi" \
-    --config-name "$CONFIG_NAME" \
-    ++name="$EXPERIMENT_NAME-pretrain" \
-    ++backbone.name="$BACKBONE"  \
-    ++data.dataset="$DATASET" 
-
-# get pretrained path from last_ckpt.txt file
 TRAINED_CHECKPOINT_PATH=$(cat last_ckpt.txt)
 TRAINED_CHECKPOINT_WANDB_ID=$(echo $TRAINED_CHECKPOINT_PATH | awk -F '/' '{print $3}')
 echo "$TRAINED_CHECKPOINT_WANDB_ID $TRAINED_CHECKPOINT_PATH"
