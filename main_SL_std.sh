9973349e878c29f726e49144be140a5a1cd673e1
mitchchessnoob <michele.narese@gmail.com>
2024-12-09 09:07:10 +0100

    Update main_SL_std.sh

--- main_SL_std.sh
+++ main_SL_std.sh
@@ -7,7 +7,7 @@ CONFIGS_PATH=semi_supervised/configs.yaml
 AUGMENTS_PATH=scripts/pretrain/office31/augmentations/asymmetric.yaml
 
 
-python3 -u main_semiSL_standard.py \
+python3 -u main_SL_standard.py \
     --config_path $CONFIGS_PATH \
     --augments_path $AUGMENTS_PATH \
 
