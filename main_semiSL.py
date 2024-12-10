aeaddca3a178ad03d177d7bcbfc51665fd5b222c
mitchchessnoob <michele.narese@gmail.com>
2024-12-10 12:03:41 +0100

    Update main_semiSL.py

--- main_semiSL.py
+++ main_semiSL.py
@@ -40,7 +40,7 @@ def main(configs_path, augments_path):
         augments = OmegaConf.create(yaml_data)
         
         # Initialize wandb
-        wandb.init(project=configs.name, name=f"SemiSL FroSSL D/A")
+        wandb.init(project=configs.name, name=f"SemiSL FroSSL A/D")
 
         # Set device
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -126,7 +126,7 @@ def main(configs_path, augments_path):
             os.makedirs(configs.checkpoint.dir, exist_ok=True)
             if test_acc > best_acc and configs.checkpoint.enabled:
                 best_acc = test_acc
-                torch.save(model.state_dict(), f"{configs.checkpoint.dir}/best_model_DA.pth")
+                torch.save(model.state_dict(), f"{configs.checkpoint.dir}/best_model_AD.pth")
 
         # Save final model
         torch.save(model.state_dict(), f"{configs.checkpoint.dir}/final_model.pth")
