3540564843b873de0dd7f6dfaffc3879ff628381
mitchchessnoob <michele.narese@gmail.com>
2024-12-12 11:00:00 +0100

    Update main_SL.sh

--- main_SL.sh
+++ main_SL.sh
@@ -4,7 +4,7 @@ export KAGGLE_USERNAME="michelenarese"
 export KAGGLE_KEY="0a593cdaf1707de5e0924de7be17019a"
 
 CONFIGS_PATH=semi_supervised/configs.yaml
-AUGMENTS_PATH=scripts/pretrain/office31/augmentations/asymmetric.yaml
+AUGMENTS_PATH=semi_supervised/asymmetric.yaml
 
 
 python3 -u main_semiSL.py \
