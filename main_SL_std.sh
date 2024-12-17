cece66a578622e894c2fcf971c39b7043d88868e
mitchchessnoob <michele.narese@gmail.com>
2024-12-17 11:06:34 +0100

    Update main_SL_std.sh

--- main_SL_std.sh
+++ main_SL_std.sh
@@ -4,11 +4,9 @@ export KAGGLE_USERNAME="michelenarese"
 export KAGGLE_KEY=#CENSORED
 
 CONFIGS_PATH=semi_supervised/configs.yaml
-AUGMENTS_PATH=scripts/pretrain/office31/augmentations/asymmetric.yaml
 
 
 python3 -u main_SL_morestandard.py \
     --config_path $CONFIGS_PATH \
-    --augments_path $AUGMENTS_PATH \
 
 echo "done"
